72c0c0d7c5264cb98857290ef7eb68c7da144d23
MayHendra <mayhendrap.10@gmail.com>
2020-06-19 08:59:10 +0700

    test commit with first config

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config({ path: '.env' });
+	require('dotenv').config();
 }
 
 const express = require('express');
@@ -19,7 +19,6 @@ const mongoose = require('mongoose');
 mongoose.connect(`${process.env.DATABASE_URL}`, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-	useCreateIndex: true,
 });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
